c6382b668b7d5db01d1493bcfc5a3e2d8bfa7920
Tom Poyeau <tompoyeau@hotmail.fr>
2023-06-27 10:34:38 +0200

    first commit

---
+++ src/test/java/org/example/BookTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.Test;
+
+public class BookTest {
+
+    @Test
+    public void CreateBookTest() {
+        Integer isbn = 1122354;
+        String titre = "Mon title";
+        String auteur = "Mon auteur";
+        String editeur = "Mon editeur";
+        String format = "Poche";
+        boolean disponible = true;
+
+        Book aBook = new Book(isbn,titre, auteur, editeur, format, disponible);
+        assertEquals(aBook.getIsbn(), isbn);
+        assertEquals(aBook.getTitre(), titre);
+        assertEquals(aBook.getAuteur(), auteur);
+        assertEquals(aBook.getEditeur(), editeur);
+        assertEquals(aBook.getFormat(), format);
+        assertEquals(aBook.getDisponible(), disponible);
+    }
+
+}
